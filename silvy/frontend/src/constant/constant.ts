d45b679621e9fbd90fcbe8d21301965c7a822596
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2025-05-21 01:23:05 +0700

    update: packageId

--- silvy/frontend/src/constant/constant.ts
+++ silvy/frontend/src/constant/constant.ts
@@ -1,9 +1,9 @@
 export const DEVNET_PACKAGE_ID = "0xTODO";
 export const TESTNET_PACKAGE_ID =
-  "0x4dcc8f8f15bca5e5455d540c689d27fc1838ba44c1a62870d9671470e87547e1";
+  "0xe93b594ec40a16b135973c52f497f1bf56a1aeacfbb5c2275385d253f1ffa57f";
 export const MAINNET_PACKAGE_ID = "0xTODO";
 
 export const ADMIN_VAULT =
-  "0x53f3d30aad39d684025447ddabdd7b282c63b2dbd7b85ba91f97d1c8e4dcb363";
+  "0xb3917060a6e6115bbd22973dc9d72fa667759d444d96214c3adeca5fc236df93";
 
 export const DOMAIN_DEV = import.meta.env.VITE_DOMAIN_DEV;
